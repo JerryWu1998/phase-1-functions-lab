38e852e46bc02d58dac6b9d6bc721f22df4e6afd
Yu Wu <yw3748@columbia.edu>
2023-05-22 15:00:04 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const length = Math.abs(destination - start) * 264;
+  if (length <= 400) {
+    return 0;
+  } else if (length > 400 && length <= 2000) {
+    return (length - 400) * 0.02;
+  } else if (length > 2000 && length <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
